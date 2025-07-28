c9b82b0fda90dcfdc052d7fd20a9e59aa8d004f2
BlackWyrm14 <185392073+BlackWyrm14@users.noreply.github.com>
2025-07-28 15:50:01 +0200

    Update Calculator.cpp
    Add the missing code from main.ccp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,4 +1,7 @@
 #include "Calculator.h"
+#include <iostream>
+
+using namespace std;
 
 double Calculator::Calculate(double x, char oper, double y) {
     switch(oper)
@@ -14,4 +17,33 @@ double Calculator::Calculate(double x, char oper, double y) {
         default:
             return 0.0;
     }
+}
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "Math error: Attempted to divide by zero!" << endl;
+
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Result " << "of " << x << oper << y << " is: " << result << endl;
+    }
+
+    return 0;
 }
